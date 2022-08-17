d63ea3ddd04dda24e31a55cb211999bb528df288
StanislavSmolyanko <st.smolyanko@gmail.com>
2022-08-17 12:34:15 +0300

    added hovers to interactive elements

--- source/js/main.js
+++ source/js/main.js
@@ -20,8 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
   changeTextButton();
   runAccordion();
 
-
-
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
